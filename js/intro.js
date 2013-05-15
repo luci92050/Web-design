984bb4e40f34fe76577b02b60e9c6165d8fd6c28
Lucian Jitaru <stres@acerubuntu.(none)>
2013-05-15 15:12:01 +0300

    intro

---
+++ js/intro.js
@@ -0,0 +1,221 @@
+//part2 -1
+// This is a comment that the computer will ignore. 
+// It is for your eyes only!
+
+"cake".length*9
+
+//part2-3
+
+// Also try the Q&A forum to get help
+// The link is on the bottom left of the page!
+
+confirm("I am learning")
+
+//part2-4
+
+prompt("Where are you from?")
+
+//part2-5
+
+"What is my name".length
+
+//part2-6
+
+"i am coding like a champ".length >10 
+
+///////////
+
+//part3-1
+
+console.log(2 * 5)
+console.log("Hello")
+
+//part3-2
+
+//After you run the code, you should see true 4 times, and false 2 times.
+//By using `console.log` at the start of each line, 
+//we are able to print 6 lines of output.
+
+console.log(15 > 4);
+console.log("Xiao Hui".length < 122);
+console.log("Goody Donaldson".length > 8);
+console.log(8*2 === 16);
+console.log("Goody Donaldson".length < 8);
+console.log(8*2 !== 16);
+
+//part3-3
+
+if ( 100 > 2 )
+{
+    console.log("You are good at math!");
+}
+
+console.log("Just letting you know: your program got to line 6");
+
+//part3-4
+
+if (0 > 1 )
+{
+    alert("Let's go down the first road!");
+}
+else
+{
+   //What should we do if the condition is false? Fill in here:
+   console.log("Elsee")
+    
+}
+
+////part3-5
+
+// Remember, the order and punctuation matter.
+// If you get an error, check carefully, line by line.
+// If you're really stuck, see the hint!
+
+if (1!==1)
+{
+    console.log("I am right")   
+}
+else
+{
+    console.log("I am wrong")
+}
+
+
+//part3-6
+
+// The computer doesn't worry about extra spaces between words or brackets
+// It just cares about the order of where things are placed
+// and that you have used the right characters (){}[]"";
+
+// For extra help, a program called a 'linter' is checking your code
+// and will put a red 'x' next to the first line that contains errors
+
+if (10 === 10)
+{
+    console.log("You got a true!")
+}
+else 
+{
+    console.log("You got a false!")
+}    
+
+////////////////////
+
+//part4-1
+
+// This is an example of an if / else statement.
+
+if (12 / 4 === "Ari".length) {
+    confirm("Will this run the first block?");
+} else {
+    confirm("Or the second block?");
+}
+
+
+//part4-2
+if ("Jon".length * 2 / (2+1) === 2 )
+{
+    console.log("The answer makes sense!");
+} 
+else 
+{
+    console.log("Error Error Error")
+}
+
+//part4-3
+// Below is an example of printing the remainder of 18/4 using modulo:
+// console.log(18 % 4); 
+
+console.log(14 % 3)
+console.log(99 % 8)
+console.log(11 % 3)
+
+//part4-4
+
+//An example of an if/else statement with modulo in the condition
+
+
+
+if( 12 % 6 ===0 ) 
+{
+    console.log("The first number is even!");
+} 
+else 
+{
+    console.log("The first number is odd!");
+}
+
+//part4 -5
+// Be careful with the substring's letter positions!
+"wonderful day".substring(3, 7)
+
+//part4-6
+// Use console.log( ) to print out the substrings.
+// Here is an example of the 1st to 4th letter of "JavaScript":
+// console.log("JavaScript".substring(0,4));
+
+console.log("January".substring(0, 3))
+console.log("Melbourne is great".substring(0, 12))
+console.log("Hamburgers".substring(3, 10))
+
+///////////////////
+//part5-1
+
+// To create a variable, we use only one equals sign
+// But to check if two values are equal, we use 3 equal signs.
+
+// declare your variable here:
+var myAge=21;
+
+console.log(myAge);
+
+//part5-2
+
+// Declare a variable on line 3 called
+// myCountry and give it a string value.
+var myCountry="Romania"
+
+// Use console.log to print out the length of the variable myCountry.
+console.log(myCountry.length );
+
+// Use console.log to print out the first three letters of myCountry.
+console.log(myCountry.substring(0, 3) );
+
+//part5-3
+
+// On line 2, declare a variable myName and give it your name.
+var myName="Stres"
+// On line 4, use console.log to print out the myName variable.
+console.log(myName)
+// On line 7, change the value of myName to be just the first 2 
+// letters of your name.
+myName=myName.substring(0, 2)
+// On line 9, use console.log to print out the myName variable.
+console.log(myName)
+
+//part5-4
+
+// On line 2, write your first comment! It can be anything!
+//comment
+var myJob="Web Developer"
+console.log(myJob.length)
+
+//part5-5
+// On line 2, write your first comment! It can be anything!
+//comment
+var myJob="Web Developer"
+console.log(myJob.length)
+
+//part5 -6
+
+// Not sure where to begin? Check the Hint!
+if (45 % 5 ===0)
+{
+    console.log("I finished my first course!")
+}
+else
+{
+    console.log("I am wrong!")
+}
+
+
